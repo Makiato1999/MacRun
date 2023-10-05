19e62ec570206f039580cec1aa5346a735f2d658
KyleQiao1992 <chocolatman1992@gmail.com>
2023-10-04 20:03:32 -0400

    init

---
+++ src/main/java/com/course/project/macrunyi_xiaoran_xue/MacrunYiXiaoranXueApplication.java
@@ -0,0 +1,13 @@
+package com.course.project.macrunyi_xiaoran_xue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacrunYiXiaoranXueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrunYiXiaoranXueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/project/macrunyi_xiaoran_xue/MacrunYiXiaoranXueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.project.macrunyi_xiaoran_xue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacrunYiXiaoranXueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
