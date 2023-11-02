8b65cc268cd92d570544e69d68a175e579e8b172
KyleQiao1992 <chocolatman1992@gmail.com>
2023-11-01 20:26:32 -0400

    add module

---
+++ game-center/src/test/java/com/course/project/gamecenter/GameCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.project.gamecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ trail_center/src/main/java/com/course/project/trail_center/TrailCenterApplication.java
@@ -0,0 +1,13 @@
+package com.course.project.trail_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailCenterApplication.class, args);
+	}
+
+}
