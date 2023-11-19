02158df5a3f44ec84a7fa562fedaa6e2b0c1d703
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-11-19 12:13:17 -0500

    complete eureka
    complete eureka

--- trail-center/src/main/java/com/course/project/trailcenter/TrailCenterApplication.java
+++ trail-center/src/main/java/com/course/project/trailcenter/TrailCenterApplication.java
@@ -1,8 +1,10 @@
 package com.course.project.trailcenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TrailCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TrailCenterApplication.class, args);
